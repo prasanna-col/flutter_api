218c2bb6cf9c747768a38b2f9fd119697a226f78
mppv <prasanna.uplogic@gmail.com>
2021-11-19 17:08:36 +0530

    index update

--- app.js
+++ app.js
@@ -33,13 +33,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
 app.use('/login',loginRouter);
 app.use('/updateProfile', updateProfileRouter);
 app.use('/getProfile', getProfileRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
